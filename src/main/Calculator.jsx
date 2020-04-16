b8aca0a17fa62db983fbb46897dc62c48f538827
Flavio Santos <flavio.santos.pro@gmail.com>
2020-04-16 16:30:55 -0300

    initial components for make calculator

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import './Calculator.css'
+import { Component } from 'react';
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className='calculator'></div>
+        )
+    }
+}
